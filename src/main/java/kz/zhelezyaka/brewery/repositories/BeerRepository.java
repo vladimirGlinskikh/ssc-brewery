6e365cd2ea23fb4d99aaa0d8f75b21c7f2b02a28
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-09-20 20:12:28 +0600

    Create an application framework for learning Spring Security

---
+++ src/main/java/kz/zhelezyaka/brewery/repositories/BeerRepository.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2020 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package kz.zhelezyaka.brewery.repositories;
+
+import kz.zhelezyaka.brewery.domain.Beer;
+import kz.zhelezyaka.brewery.web.model.BeerStyleEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+    
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
+
+    Page<Beer> findAllByBeerNameIsLike(String beerName, Pageable pageable);
+
+    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
+
+    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+
+    Beer findByUpc(String upc);
+}
