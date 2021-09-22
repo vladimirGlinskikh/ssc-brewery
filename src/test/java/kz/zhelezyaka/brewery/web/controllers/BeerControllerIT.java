44d0e74360c11d06d825e1a0cb8c7f9aebbdb382
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-09-22 21:09:16 +0600

    Testing Spring Security with JUnit 5

---
+++ src/test/java/kz/zhelezyaka/brewery/web/controllers/BeerControllerIT.java
@@ -0,0 +1,61 @@
+package kz.zhelezyaka.brewery.web.controllers;
+
+import kz.zhelezyaka.brewery.repositories.BeerInventoryRepository;
+import kz.zhelezyaka.brewery.repositories.BeerRepository;
+import kz.zhelezyaka.brewery.repositories.CustomerRepository;
+import kz.zhelezyaka.brewery.services.BeerService;
+import kz.zhelezyaka.brewery.services.BreweryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class BeerControllerIT {
+
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerRepository beerRepository;
+
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    BreweryService breweryService;
+
+    @MockBean
+    CustomerRepository customerRepository;
+
+    @MockBean
+    BeerService beerService;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser("spring")
+    @Test
+    void findBeers() throws Exception {
+        mockMvc.perform(get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+}
